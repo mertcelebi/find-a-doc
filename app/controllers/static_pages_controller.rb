11b51abc177620b4fda480708187518d4dd031f3
Mert Celebi <mertcelebi93@gmail.com>
2014-04-29 19:18:46 -0400

    Started the sign up page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,5 +7,6 @@ class StaticPagesController < ApplicationController
   def about_us
 
   end
+  
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 FindADoc::Application.routes.draw do
   
+  get "users/new"
+
   root  'static_pages#home'
+  match '/signup',  to: 'users#new', via: 'get'
   match '/about_us', to: 'static_pages#about_us', via: 'get'
 
 end
